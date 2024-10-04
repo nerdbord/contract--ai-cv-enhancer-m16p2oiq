96e8b0edff90afbb1d46db59c669b01fb21761cf
AniaPiWo <snowflake@wp.pl>
2024-10-04 11:01:34 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      external: ["@clerk/remix"],
+    },
+  },
 });
