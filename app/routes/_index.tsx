e678b6b167d42aa7bf79623415809bf3d2bc0465
AniaPiWo <snowflake@wp.pl>
2024-10-04 10:46:41 +0200

    netflify

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,41 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { MetaFunction, LoaderFunction, redirect } from "@remix-run/node";
-import { getAuth } from "@clerk/remix/ssr.server";
-import { createUserFromClerk } from "actions/user";
-import { Landing } from "~/components/ui/Landing";
-import { useLoaderData } from "@remix-run/react";
-import { MainNavigation } from "~/components/ui/MainNavigation";
+import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "AI CV Enhancer" },
-    {
-      name: "description",
-      content:
-        "AI CV Enhancer is a tool that improves and optimizes your CV using artificial intelligence.",
-    },
-    {
-      name: "keywords",
-      content:
-        "AI CV Enhancer, CV optimization, resume improvement, artificial intelligence, job application, CV builder",
-    },
-    { name: "author", content: "PA DreamTeam" },
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
-export const loader: LoaderFunction = async (args) => {
-  const { userId } = await getAuth(args);
-  if (userId) {
-    await createUserFromClerk(userId);
-  }
-
-  return { data: { userId } };
-};
-
 export default function Index() {
-  const {
-    data: { userId },
-  } = useLoaderData<{ data: { userId: string | null } }>();
-
   return (
-    <main className="flex flex-col items-center justify-start w-full flex-grow pt-20">
-      <MainNavigation />
-      <div className="flex flex-grow w-full">
-        <Landing />
-      </div>
-    </main>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
