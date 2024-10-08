6022102627ca018572cc527809b0e872b42ed7fd
AniaPiWo <snowflake@wp.pl>
2024-10-08 11:26:49 +0200

    init

--- src/components/EnhancedCV.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-export const EnhancedCV = (props: Props) => {
-  return <div>EnhancedCV</div>;
-};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: [
+          '"Inter"',
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
       },
     },
   },
-  plugins: [require("daisyui")],
-};
-export default config;
+  plugins: [],
+} satisfies Config;
